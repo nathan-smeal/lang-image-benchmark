3f093a5ea62478da03c3dd61083662e9ba713e55
Nathan Smeal <nathan.i.smeal@gmail.com>
2024-04-06 06:53:46 -0500

    c# early work

--- csharp_images/Program.cs
+++ csharp_images/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+using Emgu.CV;
 Console.WriteLine("Hello, World!");
+
+
+Mat img = CvInvoke.Imread("./images/lenna.png", Emgu.CV.CvEnum.ImreadModes.AnyColor);
+
+Console.WriteLine(img.Dims);
