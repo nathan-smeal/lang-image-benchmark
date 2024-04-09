31f554adbd258e26e1e39cd0f05065444c436977
Nathan Smeal <nathan.i.smeal@gmail.com>
2024-04-09 10:59:09 -0500

    Updated invert poc c#

--- csharp_images/Program.cs
+++ csharp_images/Program.cs
@@ -1,9 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using Emgu.CV;
-Console.WriteLine("Hello, World!");
+using Emgu.CV.Structure;
 
+String path = "images.lenna.png";
+UInt16 iterations = 101;
+// var test = csharp_images.images.images.lenna;
+// Console.WriteLine(test.Length);
+Image<Rgb, byte> img = new Emgu.CV.Image<Rgb, Byte>("C:\\Users\\smeal\\repos\\lang-image-benchmark\\csharp_images\\images\\lenna.png");
 
-Mat img = CvInvoke.Imread("./images/lenna.png", Emgu.CV.CvEnum.ImreadModes.AnyColor);
+Stopwatch stopwatch = Stopwatch.StartNew();
+for (int i = 0; i < iterations; i++)
+{
+    var out_image = img.Not();
+    // var out_image = img.Mat.GetRawData();
+    // foreach (var c in out_image)
+    // {
+    //      var test = ~c;
+    // }
+}
 
-Console.WriteLine(img.Dims);
+stopwatch.Stop();
+var time_seconds = stopwatch.Elapsed.TotalSeconds;
+Console.WriteLine(time_seconds);
+
+Console.WriteLine(img.Size);
